aadee487575bad8ae03d16f34458da50f7c6af5e
Lordwick5 <Prashantchy96@gmail.com>
2025-04-29 21:41:26 +0545

    updated code

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
+const Label1 = document.getElementById("Label1");
+const Label2 = document.getElementById("Label2");
+const Label3 = document.getElementById("Label3");
 const min = 1;
 const max = 6;
-let randomNum;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
 myButton.onclick = function(){
-    randomNum = Math.floor(Math.random() * max) + min;
-    myLabel.textContent = randomNum;
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    Label1.textContent = randomNum1;
+    Label2.textContent = randomNum2;
+    Label3.textContent = randomNum3;
 }
